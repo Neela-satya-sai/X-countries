e83134054e01c1b27635285915c7a0c8745d7f15
neelasatyasai <neelasatyasai9.3@gmail.com>
2025-05-07 12:21:24 +0530

    resolved card error

--- x-countries/src/App.jsx
+++ x-countries/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import Card from './components/card/Card'
+import Card from './components/Card/Card.jsx'
 import axios from "axios";
 
 
